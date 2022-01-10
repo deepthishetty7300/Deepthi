b1aa5ae936cb956e6c18c1ba9d86a121f7b8d5e3
niveditharai <niveditharaih@gmail.com>
2022-01-10 12:45:41 +0530

    changes

--- main.go
+++ main.go
@@ -9,26 +9,5 @@ func main() {
 	var num int
 	fmt.Scan(&num)
 
-	s := strconv.Itoa(num)
-	firstCharacter := s[0:1]
-
-	i, _ := strconv.Atoi(firstCharacter)
-
-	if i == 0 {
-		fmt.Println("Enter a Valid number")
-		main()
-
-	} else {
-
-		var s []int
-		for num > 0 {
-			s = append(s, num%10)
-			num = num / 10
-		}
-		length := len(s) - 1
-		for i := length; i > -1; i-- {
-			fmt.Print(s[i], " ")
-		}
-	}
 
 }
