dac748e1859e2880dcc255f919488574c1e01aa1
SignDesk <deeksha>
2022-01-10 14:04:59 +0530

    task1

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
+func findDigitSum(num int) int {
+
+	sum := 0
+	rem := 0
+	for num > 0 {
+		rem = num % 10
+		sum = sum + rem
+		num = num / 10
+	}
+	if sum > 9 {
+		return findDigitSum(sum)
+	}
+	return sum
+}
 func main() {
-	var num int
-	fmt.Scan(&num)
 
+	fmt.Println(findDigitSum(2462))
 
 }
