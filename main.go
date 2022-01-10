34b9b4aa656b28abf21af5eab4e5252ec5d507eb
SignDesk <deeksha>
2022-01-10 12:29:33 +0530

    version2

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func findDigitSum(num int) int {
-
-	sum := 0
-	rem := 0
-	for num > 0 {
-		rem = num % 10
-		sum = sum + rem
-		num = num / 10
-	}
-	if sum > 9 {
-		return findDigitSum(sum)
-	}
-	return sum
-}
 func main() {
+	var num int
+	fmt.Scan(&num)
 
-	fmt.Println(findDigitSum(2462))
+	s := strconv.Itoa(num)
+	firstCharacter := s[0:1]
+
+	i, _ := strconv.Atoi(firstCharacter)
+
+	if i == 0 {
+		fmt.Println("Enter a Valid number")
+		main()
+
+	} else {
+
+		var s []int
+		for num > 0 {
+			s = append(s, num%10)
+			num = num / 10
+		}
+		length := len(s) - 1
+		for i := length; i > -1; i-- {
+			fmt.Print(s[i], " ")
+		}
+	}
 
 }
