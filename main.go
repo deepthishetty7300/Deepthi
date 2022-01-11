9236dcea913831c605edee580ed28a1f19590bb2
B-Apoorva-Bhat <bhatapoorva19@gmail.com>
2022-01-11 11:09:06 +0530

    something

--- main.go
+++ main.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 
 func findDigitSum(num int) int {
-
-	sum := 0
-	rem := 0
-	for num > 0 {
-		rem = num % 10
-		sum = sum + rem
-		num = num / 10
 	}
 	if sum > 9 {
 		return findDigitSum(sum)
