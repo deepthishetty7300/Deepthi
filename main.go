8c39b218c4d790ec34485af527dc345bb504d920
SignDesk <deeksha>
2022-01-11 12:37:23 +0530

    first task

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import "fmt"
 
 func findDigitSum(num int) int {
+
+	sum := 0
+	rem := 0
+	for num > 0 {
+		rem = num % 10
+		sum = sum + rem
+		num = num / 10
 	}
-	
+	if sum > 9 {
+		return findDigitSum(sum)
+	}
+	return sum
+}
 func main() {
 
 	fmt.Println(findDigitSum(0122))
